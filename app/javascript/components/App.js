c233113be3a65b270c0332572fe0cef02c48f6cf
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-03-24 15:34:29 -0700

    add imports and paths for render

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import HelloWorld from './HelloWorld'
+
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => ("Home!")} />
+          <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
+        </Switch>
+      </BrowserRouter>
+
     );
   }
 }
