a35f22a08cbdec38e62c0c3d820ba060dbbf8e5e
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-03-24 15:37:49 -0700

    Forward all requests to staticcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML # Mime type (req.format.html?)
+  # This does not include the root ("/") path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+  # Forward root to StaticController#index
+  root 'static#index'
 end
