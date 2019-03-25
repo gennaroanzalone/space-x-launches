ec4a8c95cae1a0005e023a4d9d2a6f23ed842e15
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-03-24 17:31:43 -0700

    let reducer handle success action and return new state from Rails API

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -14,9 +14,10 @@ const initialState = {
 function rootReducer(state, action) {
   console.log(action.type);
   switch (action.type) {
-    default:
-      return state
+    case "GET_THINGS_SUCCESS":
+      return { things: action.json.things };
   }
+  return state;
 }
 
 export default function configureStore() {
